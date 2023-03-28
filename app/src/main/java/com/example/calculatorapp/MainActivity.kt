1dcb748dd2413ba22d2d4058674ecf7874e01eda
Sam Maya <samuelmaya112@gmail.com>
2023-03-28 18:36:31 -0400

    finished functionality

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -61,4 +61,52 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    fun calculate(view : View){
+        if(hasOperator && lastNum){
+            val str : CharSequence = display?.text as CharSequence
+            val operatorsList = arrayListOf('+', '-', '*', '/')
+            val nums = arrayListOf<String>()
+            var currentStr = ""
+            var op : Char? = null
+            var i = 0
+
+            while(i < str.length){
+                if (str[i] in operatorsList){
+                    if (str[i] == '-' && currentStr == "") {
+                        currentStr += str[i]
+                        i++
+                    }else {
+                        nums.add(currentStr)
+                        currentStr = ""
+                        op = str[i]
+                        i++
+                    }
+                }else {
+                    currentStr += str[i]
+                    i++
+                }
+            }
+            nums.add(currentStr)
+
+            var ans = nums[0].toDouble()
+            when (op){
+                '*' -> ans *= nums[1].toDouble()
+                '/' -> if(nums[1].toDouble() != 0.0){
+                    ans /= nums[1].toDouble()
+                }
+                '+' -> ans += nums[1].toDouble()
+                '-' -> ans -= nums[1].toDouble()
+            }
+            if(ans % 1 == 0.0){
+                display?.text = ans.toInt().toString()
+            }else display?.text = ans.toString()
+
+            hasOperator = false
+            lastDecimal = true
+            if(ans < 0){
+                isNeg = true
+            }
+        }
+    }
 }
